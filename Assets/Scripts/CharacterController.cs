7f0ed0d54235bd150ed307f9114767803cccdfcc
Jitesh <jitesh.jariwala02@gmail.com>
2020-02-01 04:19:06 +0530

    Player controller added with scene

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerType playerType;
+
+    [SerializeField]
+    private Rigidbody2D playerRigidbody;
+
+    [SerializeField]
+    private KeyBinding keyBinding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(keyBinding.moveUp))
+        {
+            MovePlayer(MoveDirection.UP);
+        }
+        if (Input.GetKey(keyBinding.moveDown))
+        {
+            MovePlayer(MoveDirection.DOWN);
+        }
+        if (Input.GetKey(keyBinding.moveLeft))
+        {
+            MovePlayer(MoveDirection.LEFT);
+        }
+        if (Input.GetKey(keyBinding.moveRight))
+        {
+            MovePlayer(MoveDirection.RIGHT);
+        }
+    }
+
+    private void MovePlayer (MoveDirection direction)
+    {
+        switch(direction)
+        {
+            case MoveDirection.UP:
+                playerRigidbody.AddForce(new Vector2(0, 1));
+                break;
+            case MoveDirection.DOWN:
+                playerRigidbody.AddForce(new Vector2(0, -1));
+                break;
+            case MoveDirection.LEFT:
+                playerRigidbody.AddForce(new Vector2(-1, 0));
+                break;
+            case MoveDirection.RIGHT:
+                playerRigidbody.AddForce(new Vector2(1, 0));
+                break;
+        }
+    }
+
+
+}
+
+[System.Serializable]
+public class KeyBinding
+{
+    public string moveUp;
+    public string moveDown;
+    public string moveLeft;
+    public string moveRight;
+}
+
+public enum MoveDirection
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+public enum PlayerType
+{
+    PLAYER_ONE,
+    PLAYER_TWO
+}
